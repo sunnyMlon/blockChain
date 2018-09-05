88f137a6693e27924b5d3d1413f8618ad5e4dec0
sunnyMlon <zhangdoyourbest@163.com>
2018-09-05 12:35:56 +0800

    Update block.php
    Optimize the code  random_str

--- blockchain/block.php
+++ blockchain/block.php
@@ -15,15 +15,15 @@ class block {
     private $timestamp;
     private $info;
     private $previous_hash;
-    private $random_str;
+    private $previous_hash_encrypt;
     private $hash;
 
-    public function __construct($index, $timestamp, $info, $random_str, $previous_hash) {
+    public function __construct($index, $timestamp, $info, $previous_hash_encrypt, $previous_hash) {
         $this->index = $index;
         $this->timestamp = $timestamp;
         $this->info = $info;
         $this->previous_hash = $previous_hash;
-        $this->random_str = $random_str;
+        $this->previous_hash_encrypt = $previous_hash_encrypt;
         $this->hash = $this->hash_block();
     }
 
@@ -32,14 +32,14 @@ class block {
     }
 
     private function hash_block() {
-        $str = $this->index . $this->timestamp . $this->info . $this->random_str . $this->previous_hash;
+        $str = $this->index . $this->timestamp . $this->info . $this->previous_hash_encrypt . $this->previous_hash;
         return hash("sha256", $str);
     }
 }
 
 /**
  * 创造区块链头
- * @return block
+ * @desc  用于创建区块链的链头
  */
 function create_block_header() {
     return new block(0, time(), "第一个区块", 0, 0);
@@ -47,14 +47,14 @@ function create_block_header() {
 
 /**
  * 挖矿，生成下一个区块
- * 这应该是一个很复杂的算法，但为了简单，我们这里挖到前1位是数字就挖矿成功。
+ * @desc 实际使用中，这里是一个很复杂的算法，此处做了简化，如果这里挖到前1位是数字的就算挖矿成功。
  */
 function dig_new_blcok($last_block_obj) {
-    $random_str = $last_block_obj->hash . get_random();
+    $previous_hash_encrypt = $last_block_obj->hash . get_random();
     $index = $last_block_obj->index + 1;
     $timestamp = time();
     $info = 'Hello, My name is block_' . $index;
-    $block_obj = new block($index, $timestamp, $info, $random_str, $last_block_obj->hash);
+    $block_obj = new block($index, $timestamp, $info, $previous_hash_encrypt, $last_block_obj->hash);
     //前一位不是数字
     if (!is_numeric($block_obj->hash{0})) {
         return false;
@@ -65,20 +65,24 @@ function dig_new_blcok($last_block_obj) {
 
 /**
  * 验证区块
- * @desc 这也是一个复杂的过程，为了简单，我们这里直接返回正确
+ * @desc 实际应用中，这也是一个复杂的算法过程，这里做了简化，如果传入的是对象我们这里就算校验通过
  */
 function verify($last_block_obj) {
+    if (empty($last_block_obj) || !is_object($last_block_obj)) {
+        return false;
+    }
     return true;
 }
 
 /**
  * 生成随机字符串
+ * @desc  加密字符串，由上一个区块的hash值和当前区块的随机字符组成, 用于给当前区块展示上一区块的唯一hash信息，但是需要反解密才能看到真实数据
  */
 function get_random($len = 32) {
     $str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $key = "";
     for ($i = 0; $i < $len; $i++) {
-        $key .= $str{mt_rand(0, 32)};//随机数
+        $key .= $str{mt_rand(0, $len)};//随机数
     }
     return $key;
 }
@@ -86,11 +90,11 @@ function get_random($len = 32) {
 header("Content-type:text/html;charset=utf-8");
 //生成第一个区块
 $blockchain = (array)create_block_header();
-//模拟生成其他区块,我们直接循环生成。实际中，还需要跟踪互联网上多台机器上链的变化,像比特币会有工作量证明等算法，达到条件了才生成区块等
+//模拟生成其他区块,我们直接循环生成。实际中，还需要跟踪互联网上多台机器上链的变化,像比特币会生成有工作量证明、破译时长等算法，达到条件才能生成区块
 //我们的链是一个数组，实际生产中应该保存下来
 $previous_block = $blockchain[0];
 for ($i = 0; $i <= 10; $i++) {
-    if (!($new_block = dig_new_blcok($previous_block))) {
+    if (!($new_block = dig_new_blcok($previous_block)) || !verify($new_block)) {
         continue;
     }
     $blockchain[] = $new_block;
